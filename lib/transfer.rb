5a73d4d8b23591d4a53a16d951e5d42dea3f81b6
mathlete01 <msallin@gmail.com>
2020-10-08 14:57:22 -0700

    Solved the lab

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,31 @@
-class Transfer
-  # your code here
+lass Transfer
+  attr_accessor :status
+  attr_reader :sender, :receiver, :amount
+  def initialize(sender,receiver,amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = 'pending'
+  end
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+  def execute_transaction
+    #binding.pry
+    if valid? && self.status == "pending" && self.sender.balance >= amount
+      sender.balance = sender.balance - amount
+      receiver.balance = receiver.balance + amount
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+  def reverse_transfer
+    if self.status == 'complete'
+      receiver.balance = receiver.balance - amount
+      sender.balance = sender.balance + amount
+      self.status = 'reversed'
+    end
+  end
 end
